4565535fa3611c7537db97943e1bd98b783cb81d
Trung Thai <t35thai@yahoo.com>
2019-01-16 16:37:57 -0500

    add MapBox API keys to the project, completing project task2 by improving app responsiveness, convering Reviews, image, restaurant item list,etc..

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoidDM1dGhhaSIsImEiOiJjanF5aGs4eDMwM3JpNDRwOTVmdDU1bGlqIn0.tBpUEBZOWO7RPCWA0b2Hbg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
